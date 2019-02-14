19f52e2e022acaca9ff81bf2774d9bb44be398d5
ad0cid <ad0raci0ncid@gmail.com>
2019-02-14 12:25:31 -0600

    estructura del proyecto

---
+++ miJs.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el:,
+    data:{
+
+    },
+    methods:{
+
+    }
+})
+
+/*codigo para bloquear que vaya la pagina para atras
+
+window.location.hash="no-back-button";
+window.location.hash="Again-No-back-button";//esta linea es necesaria para chrome
+window.onhashchange=function(){window.location.hash="no-back-button";}
+
+
+*/
