8b4f62d49d2a90f1917a3fae0c8bcabe5bee6c6d
ad0cid <ad0raci0ncid@gmail.com>
2019-02-18 10:24:11 -0600

    agregue vista de agradecimiento

--- miJs.js
+++ miJs.js
@@ -7,12 +7,3 @@ const app = new Vue({
 
     }
 })
-
-/*codigo para bloquear que vaya la pagina para atras
-
-window.location.hash="no-back-button";
-window.location.hash="Again-No-back-button";//esta linea es necesaria para chrome
-window.onhashchange=function(){window.location.hash="no-back-button";}
-
-
-*/
